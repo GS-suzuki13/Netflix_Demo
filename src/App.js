d4ca41388d6b33c6ef54f5a1b7f674d74cc1d627
GS-suzuki13 <gustavosuzuki3@gmail.com>
2021-09-28 16:30:51 -0300

    Comitando criacao de componentes, organizacao de pastas e arquivos, criacao de desings e organizacao das listas dentro do site

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, {useEffect, useState} from "react";
+import './App.css';
 import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow/MovieRow";
+import FeatureMovie from "./components/FeatureMovies/FeatureMovie";
+import Header from "./components/Header/Header";
+
 
 
 export default () => {
     
     const [movieList, setMovieList] = useState([]);
+    const [featureData, setFeatureData] = useState(null);
 
     useEffect(() => {
         const loadAll = async () => {
             let list = await Tmdb.getHomeList();
             setMovieList(list);
+
+            let originals = list.filter(i=>i.slug === 'originals');
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+            let chosen = originals[0].items.results[randomChosen];
+            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+            setFeatureData(chosenInfo)
         }
 
         loadAll();
@@ -18,11 +30,16 @@ export default () => {
 
     return (
         <div className="page">
-            <section className="list">
+
+            <Header />
+
+            {featureData &&
+                <FeatureMovie item={featureData} />
+            }
+
+            <section className="lists">
                 {movieList.map((item, key)=>(
-                    <div>
-                        {item.title}
-                    </div>
+                  <MovieRow key={key} title={item.title} items={item.items} />  
                 ))}
             </section>
         </div>
